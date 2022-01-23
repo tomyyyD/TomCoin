7c7b1e2ca1b0f616aa03ca1438905ed1f380fbfe
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2022-01-22 23:29:17 -0500

    Updates to the hosting
    I need to figure out how to essentially make every node have a copy of the database that contains the chain.

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask import render_template
+from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask ("BritCoin server")
 
+# initialize firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('todos')
+
+@app.route('/add', methods=['POST'])
+def create():
+    try:
+        id = request.json['id']
+        todo_ref.document(id).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"an error occurred: {e}"
+
 @app.route('/')
 def hello ():
-    return render_template('homepage.html', )
+    return render_template('homepage.html')
